7cd21a96eba3d3176d47bab366217764461eb2e0
raihanramadhan-17 <101073974+raihanramadhan-17@users.noreply.github.com>
2022-03-14 16:13:02 +0700

    Add files via upload

---
+++ homepage2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+    <header>
+    	<div class="wrapper">
+    		<div class="logo">
+    			<img src="images/logo.png" alt="">
+    		</div>
+    		<ul class="nav-area">
+    			<li><a href="#">home</a></li>
+    			<li><a href="#">about</a></li>
+    			<li><a href="#">services</a></li>
+    			<li><a href="#">portofolio</a></li>
+    			<li><a href="#">contact</a></li>
+    		</ul>
+    	</div>
+    	
+    	<div class="welcome-text">
+    		<h1>Hi Raihan</h1>
+    		<a href="#">contact</a>
+    	</div>	
+    </header>
+</body>
+</html>
